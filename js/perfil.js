a8b3be141084b7059ef80227c4e0d7a8916c0687
AdriGuti-rgb <adriangutusoz65@gmail.com>
2024-01-15 23:50:26 +0100

    Perfil

--- js/perfil.js
+++ js/perfil.js
@@ -18,3 +18,14 @@ localidades
 
 e.target.removeEventListener("click", anadirLocalidades)
 }
+
+// Obtener el elemento
+var opcionesPerfil = document.getElementById('opcionesPerfil');
+
+// Función para mostrar u ocultar el menú
+function toggleMenu() {
+    opcionesPerfil.classList.toggle('mostrar');
+}
+
+var botonMostrarOpciones = document.getElementById('perfil');
+botonMostrarOpciones.addEventListener('click', toggleMenu);
